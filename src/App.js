9002a50e1f21943b54acac37dad40536c2867335
FNuri <fowsihashi@hotmail.co.uk>
2020-09-12 16:43:03 +0300

    added content to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ExpenseList from './components/ExpenseList'
 import ExpenseForm from './components/ExpenseForm'
 import Alert from './components/Alert'
+import uuid from 'uuid/v4'
+
+const initialExpenses = [
+
+  {id: uuid(), charge: "rent", amount: 1600},
+  {id: uuid(), charge: "car payment", amount: 400},
+  {id: uuid(), charge: "credit card payment", amount: 1200}
+
+]
+
 
 function App() {
+
+  const [expenses, setExpenses] = useState(initialExpenses)
+  
   return (
     
     <>
   
   <Alert/>
+  <h1>Budget Calculator</h1>
+  <main className="App">
   <ExpenseForm/>
-  <ExpenseList/>
+  <ExpenseList expenses={expenses}/>
+  </main>
+  <h1>
+    total spending: <span className="total">
+    £{expenses.reduce((acc, curr) => {
+      return (acc += curr.amount)
+    }, 0)}
+    </span>
+  </h1>
+  
 
 
     </>
