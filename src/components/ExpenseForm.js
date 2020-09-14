1cc66352eb043e7db6fc767cf0c25f1384871723
FNuri <fowsihashi@hotmail.co.uk>
2020-09-14 13:10:10 +0300

    Added react icons

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import {MdSend} from 'react-icons/md'
 const ExpenseForm = () => {
   return (
-    <div>
-      Hello from expense form
-    </div>
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input 
+          type="text" 
+          className="form-control" 
+          id="amount" 
+          name="amount" 
+          placeholder="e.g 100"
+          
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="charge">Charge</label>
+          <input 
+          type="text" 
+          className="form-control" 
+          id="charge" name="charge" 
+          placeholder="e.g rent"
+          
+          />
+        </div>
+      </div>
+
+      <button type="submit" className="btn">
+        Submit
+        <MdSend className="btn-icon"/>
+      </button>
+
+    </form>
   )
 }
 
