34662b367677030a1e40cc766765efe5f76766f6
FNuri <fowsihashi@hotmail.co.uk>
2020-09-16 10:46:02 +0300

    Added delete item functionality

--- src/App.js
+++ src/App.js
@@ -76,11 +76,14 @@ function App() {
   // clear all items
   const clearItems = () => {
     setExpenses([])
+    handleAlert({type: 'danger', text: 'all items deleted'})
   }
 
   // handle delete
   const handleDelete = (id) => {
-    console.log(`item deleted : ${id}`)
+    let tempExpenses = expenses.filter(item => item.id !== id)
+    setExpenses(tempExpenses)
+    handleAlert({type: 'danger', text: 'item deleted'})
   }
 
    // handle edit
